f798ef68affdb7c7b881b5c864b3caf38cf82acb
Amandeep Sarow <asarow@ucsd.edu>
2015-08-10 17:47:23 -0700

    Successfully pulls stock price from API

--- DataGrabber.py
+++ DataGrabber.py
@@ -1,9 +1,21 @@
+import urllib
+from urllib import request
 
 class DataGrabber:
     dataURL = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from" \
               "%20yahoo.finance.quote%20where%20symbol%20in%20(%22"
-    secondURL = "YHOO%22)&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2" \
+    secondURL = "%22)&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2" \
                 "Falltableswithkeys"
 
     def __init__(self):
-        print(self.dataURL)
+        pass
+        
+    def grabPrice(self, ticker):
+        page = urllib.request.urlopen(self.dataURL + ticker + self.secondURL)
+        for line in page:
+            data = str(line, encoding='utf8')
+            if "<LastTradePriceOnly" in data:
+                firstIndex = data.find("<LastTradePriceOnly>")
+                secondIndex = data.find("</LastTradePriceOnly>")
+                print(data[firstIndex+20:secondIndex])
+                break
